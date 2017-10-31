502bd4c524633f58c4de3fc5c3871b37b5f2edfc
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-10-31 14:32:30 -0600

    Aspirantes Avances

---
+++ templates/aspirantes/informacion.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<title>CETI COLOMOS</title>
+		<!--  META TAGS -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+		<!--  ESTILOS  -->
+        <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../../css/carousel.css">
+        <link rel="stylesheet" type="text/css" href="../../css/dropdown.css">
+        <link rel="stylesheet" type="text/css" href="../../css/imagegallery.css">
+		<link rel="stylesheet" type="text/css" href="../../css/principales.css">
+        <!-- Bootstrap Dropdown Hover CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+	</head>
+
+	<body>
+        <!-- MENU PRINCIPAL -->
+        <?php include_once('../../menu.html'); ?>
+
+        <!-- CONTENIDO A PARTIR DE AQUI -->
+        <main class="principal">
+        
+        <!--  CONTENIDO PÁGINA -->
+         
+		  <div class='container nota interior ficha'>
+		    					<div class='col-md-12 col-sm-12 col-xs-12'>
+					                <ol class='breadcrumb'>
+					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
+					                    <li><a href='../../index.php'>Inicio</a></li>
+					                    <li>Aspirantes</li>
+					                    <li><a href='/WebColomosGob/templates/aspirantes/cursoPropedeutico.php'>Curso Propedéutico</a></li>
+					                    <li class='active'><a href='/WebColomosGob/templates/aspirantes/Información.php'>Información</a></li>
+					                </ol>
+					            </div>
+								<div class='row'><div class='col-sm-7 pull-left'>
+					            		<h2 class='bottom-buffer'>Información General</h2>
+
+					            	</div>
+					            	<div class='col-md-4 col-xs-12 pull-right '>
+									    <!--imagen por encima del menu lateral-->
+					            		<img class='img img-responsive' src="../../imagenes/aspirantes/curso.jpg" alt="Anuies">
+								    </div>
+
+								    <div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<hr class='red'>
+								    		<p class='contenido'>
+                                             <h4>Inscripciones</h4>
+                                             <p>La inscripción al curso propedéutico únicamente es en 2 periodos:<p>
+                                             <ul type = circle>
+                                             <li>Enero para ingresar al CETI en el periodo de Agosto</li>
+                                             <li>Julio-Agosto para ingresar al CETI en el periodo de Febrero</li>
+                                         	</ul>
+
+                                             <h4>Calendario de Inscripciones</h4>
+                                             <p>Cursos Propedéutico Febrero-Junio 2017</p>
+                                             <p>Inscripción: 09 de Enero al 03 de Febrero de 2017</p>
+                                             <p>Inicio de curso: 11 de Febrero</p>
+                                             <p>Fin de curso: 17 de Junio</p>
+                                             <p>Costo: $ 1,145.00</p>
+                                             <p>Sábados de clases de 8:00 a 13:00 hrs.</p>
+
+                                             <h4>Requisitos</h4>
+                                             <p>Solicitud al Curso</p>
+
+                                             <p>Ficha de Pago de Inscripción<p>
+
+                                             <p>2 Fotografías Tamaño Credencial a Blanco y Negro de Foto Estudio (NO INSTANTÁNEAS).</p>
+                                             <ul type = circle>
+                                             <li>Requisitos <button class="btn btn-primary">Consultar</button></li>
+                                             <li>Solicitud del curso <button class="btn btn-primary">Consultar</button></li>
+                                         	</ul>
+								    	</div>
+								    </div>
+								 <div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<hr class=''>
+								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-10-31 19:49:10</p>
+								    	</div>
+								    </div>
+		    					</div>
+
+	    					</div>
+		<br>
+        <br>
+        <br>
+        </main>
+
+        <!-- FINAL CONTENIDO -->
+		<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+        <script src="js/jquery.js"></script>
+
+        <!-- Dropdown -->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Bootstrap Dropdown Hover JS -->
+        <script src="js/bootstrap-dropdownhover.min.js"></script>
+	</body>
+</html>
