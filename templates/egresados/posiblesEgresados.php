e3207741f703f9d86e91205a9df869af79815750
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-11-16 14:31:05 -0600

    Egresados Avances

---
+++ templates/egresados/posiblesEgresados.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<title>CETI COLOMOS</title>
+		<!--  META TAGS -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+		<!--  ESTILOS  -->
+        <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../../css/carousel.css">
+        <link rel="stylesheet" type="text/css" href="../../css/dropdown.css">
+        <link rel="stylesheet" type="text/css" href="../../css/imagegallery.css">
+		<link rel="stylesheet" type="text/css" href="../../css/principales.css">
+        <!-- Bootstrap Dropdown Hover CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+	</head>
+
+	<body>
+        <!-- MENU PRINCIPAL -->
+        <?php include_once('../../menu.html'); ?>
+
+        <!-- CONTENIDO A PARTIR DE AQUI -->
+        <main class="principal">
+        
+        <!--  CONTENIDO PÁGINA -->
+         
+		  <div class='container nota interior ficha'>
+		    					<div class='col-md-12 col-sm-12 col-xs-12'>
+					                <ol class='breadcrumb'>
+					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
+					                    <li><a href='../../index.php'>Inicio</a></li>
+					                    <li>Egresados</li>
+					                    <li class='active'><a href='/WebColomosGob/templates/Egresados/posiblesEgresados.php'>Posibles Egresados</a></li>
+					                </ol>
+					            </div>
+								<div class='row'><div class='col-sm-7 pull-left'>
+					            		<h2 class='bottom-buffer'>Listas de Posibles Egresados</h2>
+
+					            	</div>
+					            	<div class='col-md-4 col-xs-12 pull-right '>
+									    <!--imagen por encima del menu lateral-->
+					            		<img class='img img-responsive' src="../../imagenes/aspirantes/curso.jpg" alt="Anuies">
+                                        <!--Menu lateral -->
+                                        <div class='border-box submenu_relacionados'>
+                                          <h4 class='bottom-buffer'>Sitios relacionados</h4>
+                                            <hr class='red sitios_relacionados'>
+                                            <ul>
+                                            <li><a href="http://www.colomos.ceti.mx/">Lista de Egresados</a><hr class="sub-hr"></li>   
+                                            </ul>
+                                        </div> 
+								    </div>
+
+								    <div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<hr class='red'>
+								    		<p class='contenido'>
+								    			<h4>Periodo Agosto - Diciembre 2017</h4>
+								    		</p>
+                                            <p>Nivel Tecnologo <a href="/WebColomosGob/documentos/egresados/32017PosiblesEgresadosTgo_Web_ok.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a><p>
+                                            <p>Nivel Ingeniería <a href="/WebColomosGob/documentos/egresados/32017PosiblesEgresadosIng_Web_ok.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a></p>
+								    	</div>
+								    </div>
+								 <div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<hr class=''>
+								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-11-16 14:24:47</p>
+								    	</div>
+								    </div>
+		    					</div>
+
+	    					</div>
+		<br>
+        <br>
+        <br>
+        </main>
+
+        <!-- FINAL CONTENIDO -->
+		<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+        <script src="js/jquery.js"></script>
+
+        <!-- Dropdown -->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Bootstrap Dropdown Hover JS -->
+        <script src="js/bootstrap-dropdownhover.min.js"></script>
+	</body>
+</html>
