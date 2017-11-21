fb3fcbc4aebbb5fa2d6b7d4a39f1ed5ed8ff2599
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-11-21 14:45:36 -0600

    Docentes avances

--- templates/egresados/egresados.php
+++ templates/egresados/egresados.php
@@ -33,11 +33,11 @@
 					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
 					                    <li><a href='../../index.php'>Inicio</a></li>
 					                    <li>Egresados</li>
-					                    <li class='active'><a href='/WebColomosGob/templates/Egresados/posiblesEgresados.php'>Posibles Egresados</a></li>
+					                    <li class='active'><a href='/WebColomosGob/templates/Egresados/egresados.php'>Egresados</a></li>
 					                </ol>
 					            </div>
 								<div class='row'><div class='col-sm-7 pull-left'>
-					            		<h2 class='bottom-buffer'>Listas de Posibles Egresados</h2>
+					            		<h2 class='bottom-buffer'>Listas de Egresados</h2>
 
 					            	</div>
 					            	<div class='col-md-4 col-xs-12 pull-right '>
@@ -48,7 +48,7 @@
                                           <h4 class='bottom-buffer'>Sitios relacionados</h4>
                                             <hr class='red sitios_relacionados'>
                                             <ul>
-                                            <li><a href="/WebColomosGob/templates/Egresados/egresados.php">Lista de Egresados</a><hr class="sub-hr"></li>   
+                                            <li><a href="/WebColomosGob/templates/Egresados/posiblesEgresados.php">Lista de Posibles Egresados</a><hr class="sub-hr"></li>   
                                             </ul>
                                         </div> 
 								    </div>
@@ -57,16 +57,28 @@
 								    	<div class='article-body'>
 								    		<hr class='red'>
 								    		<p class='contenido'>
-								    			<h4>Periodo Agosto - Diciembre 2017</h4>
+								    			<h4>Periodo Febrero - Junio 2017</h4>
 								    		</p>
-                                            <p>Nivel Tecnologo <a href="/WebColomosGob/documentos/egresados/32017PosiblesEgresadosTgo_Web_ok.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a><p>
-                                            <p>Nivel Ingeniería <a href="/WebColomosGob/documentos/egresados/32017PosiblesEgresadosIng_Web_ok.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a></p>
+								    		<p class="contenido">
+								    			Graduación Tecnólogo e Ingeniería 22 de Septiembre del 2017.
+								    			<br>
+								    			<br>
+								    			Los egresados deben presentarse hora y media antes del evento para la toma de fotografías de generación.
+								    			<br>
+								    			<br>
+								    			Tecnólogo: 10:00 hrs.
+								    			<br>
+												Ingeniería: 16:00 hrs.
+								    		</p>
+								    		<h4>Listas de Consulta</h4>
+                                            <p>Nivel Tecnologo <a href="/WebColomosGob/documentos/egresados/ListaDefinitivaEgresadosTgoFeb-Jun2017.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a><p>
+                                            <p>Nivel Ingeniería <a href="/WebColomosGob/documentos/egresados/ListadedefinitivadeEgresadosIngFeb-Jun2017_ok_Web_Caso1.pdf" target="_blank" class="btn btn-primary pull-right" >Consultar</a></p>
 								    	</div>
 								    </div>
 								 <div class='col-sm-7 pull-left'>
 								    	<div class='article-body'>
 								    		<hr class=''>
-								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-11-16 14:24:47</p>
+								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-11-21 11:24:47</p>
 								    	</div>
 								    </div>
 		    					</div>
