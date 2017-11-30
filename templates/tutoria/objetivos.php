66658296114ab4989c77a73d77a2f2b5145306ce
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-11-30 14:41:20 -0600

    Tutorias Avances

--- templates/tutoria/objetivos.php
+++ templates/tutoria/objetivos.php
@@ -33,11 +33,12 @@
 					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
 					                    <li><a href='../../index.php'>Inicio</a></li>
 					                    <li>Desarrollo Académico</li>
-					                    <li class='active'><a href='/WebColomosGob/templates/tutoria/tutorias.php'>Tutorías</a></li>
+					                    <li><a href='/WebColomosGob/templates/tutoria/tutorias.php'>Tutorías</a></li>
+					                    <li class='active'><a href='/WebColomosGob/templates/tutoria/objetivos.php'>Objetivo</a></li>
 					                </ol>
 					            </div>
 								<div class='row'><div class='col-sm-7 pull-left'>
-					            		<h2 class='bottom-buffer'>Tutorías</h2>
+					            		<h2 class='bottom-buffer'>Objetivo</h2>
 
 					            	</div>
 					            	<div class='col-md-4 col-xs-12 pull-right '>
@@ -48,7 +49,7 @@
 									        <h4 class='bottom-buffer'>Submenú de la sección</h4>
 									        <hr class='red sitios_relacionados'>
 									       	<ul>	
-									       		<li><a href="/WebColomosGob/templates/tutoria/objetivos.php">Objetivo</a><hr class="sub-hr"></li>	
+									       		<li><a href="/WebColomosGob/templates/tutoria/tutorias.php">Tutorías</a><hr class="sub-hr"></li>	
 											    <li><a href="/WebColomosGob/templates/tutoria/materialApoyo.php">Directorio</a><hr class="sub-hr"></li>
 											    <li><a href="/WebColomosGob/templates/tutoria/Informacion.php">Docentes Tutores</a><hr class="sub-hr"></li>	
 											    <li><a href="/WebColomosGob/templates/tutoria/materialApoyo.php">Construye-T</a><hr class="sub-hr"></li>
@@ -76,28 +77,35 @@
 								    <div class='col-sm-7 pull-left'>
 								    	<div class='article-body'>
 								    		<hr class='red'>
-								    		<h4>¿Qué son las Tutorías?</h4>
+								    		<h4>Objetivo General</h4>
 								    		<p class='contenido'>
-                                             Las tutorías son un proceso de acompañamiento de tipo grupal, académico y focal que realizará el tutor para mejorar el rendimiento escolar de los alumnos y así tratar de prevenir la deserción escolar, al tiempo que contribuye en la formación del alumno en aspectos personales y sociales a lo largo de su carrera de tecnólogo.
+                                             Apoyar a los alumnos del CETI en su recorrido escolar, con el propósito de prevenir el posible fracaso escolar de los alumnos que se encuentren en riesgo y así incidir en el mejoramiento de su desempeño académico y formación integral
 											</p>
-											<h4>Tutores</h4>
+											<h4>Objetivos Particulares</h4>
 											<p class='contenido'>
-												El tutor es un profesor encargado de brindar apoyo académico y/o personal necesario al alumno que le ha sido encomendado y de crear un ambiente adecuado de confianza y respeto para su desarrollo ayudándole a prevenir posibles desajustes que se puedan presentar a lo largo de su trayectoria dentro de la Institución.
-												La tutoría lleva implícito un compromiso en el que el tutor está atento al desarrollo del estudiante, mientras que el tutorado debe desempeñar el papel más activo como actor de su propio aprendizaje, todo en el marco de una relación estrecha entre ambos, más que la establecida en un aula durante un curso normal.
-												<br>
-												<br>
-												La misión del tutor es brindar orientación educativa y motivacional al estudiante en lo que respecta básicamente su vida escolar, atendiendo problemas personales de aprendizaje, promoviendo el desarrollo de las potencialidades de los estudiantes y consolidando el perfil del futuro profesionista.
-												<br>
-												<br>
-												El tutor juega un papel importante en el proyecto educativo, ya que apoya a los estudiantes en actitudes como la de crear en ellos la necesidad de capacitarse, de explorar aptitudes; de mejorar su aprendizaje y tomar conciencia de manera responsable de su futuro, la tarea del tutor, entonces, consiste en estimular las capacidades y procesos de pensamiento, de toma de decisiones y de resolución de problemas.
-											</p>
-											<h4>Tipos de Tutorías</h4>
-											<h5>Tutoria Grupal</h5>
-											<p class="contenido">
-												Esta tutoría es el primer contacto con los alumnos, se atienden problemáticas grupales, involucra y da atención a los padres de familia, lleva a cabo la detección directa de situaciones de riesgo o sobresalientes de los alumnos,  brinda asesorías básicas de orientación e información, canaliza en caso de ser necesario, da seguimiento, está en contacto con profesores y áreas o departamentos involucrados con la acción tutorial.
-												<br>
-												<br>
-												Uno de sus objetivos, es que a través de la implementación de las dinámicas del manual ConstruyeT para tutores, se aborden en el grupo las seis dimensiones que se proponen y así contribuir a que el alumno tenga un mayor conocimiento de sí mismo, una vida saludable, se vincule la escuela con la familia, se aumente la participación juvenil, se fomente la inclusión, solidaridad y cooperación para la solución pacifica de conflictos y por último, se favorezca el desarrollo de competencias en las y los jóvenes para que definan y conduzcan su futuro, de manera de que se hagan dueños de su vida, descubran sus valores, capacidades, potencialidades y el sentido de su existencia.
+												<ul type = circle>
+													<li>
+														Integrar a los alumnos a un nuevo entorno escolar, ya sea que inicien sus estudios de bachillerato o que se hayan cambiado de otra institución, con el fin de fomentar su sentido de pertenencia a la escuela.
+													</li>
+													<li>
+														Detectar oportunamente las problemáticas académicas y/o personales de los alumnos, previa a la evaluación parcial o final, para implementar estrategias pertinentes que den solución o permitan su canalización a tiempo a las instancias correspondientes.
+													</li>
+													<li>
+														Dar seguimiento y apoyo a los alumnos en relación con sus procesos de aprendizaje y desarrollo personal, con el propósito de mejorar su desempeño académico y trayectoria escolar.
+													</li>
+													<li>
+														Apoyar a los alumnos a nivel pedagógico, psicológico y/o académico, con la colaboración de profesores, tutores y administrativos comprometidos con la formación integral de los jóvenes, con el propósito de mejorar su rendimiento escolar, así como su estancia en la Institución.
+													</li>
+													<li>
+														Construir el historial no académico del alumno, con el propósito de facilitar la labor del tutor en el acompañamiento de sus tutorados.
+													</li>
+													<li>
+														Capacitar a los tutores para la mejora continua de su labor frente a los alumnos.
+													</li>
+													<li>
+														Promover la atención oportuna y eficiente a los padres de familia a través de reuniones periódicas con los tutores de grupo, con la finalidad de involucrarlos en la mejora de desempeño escolar de sus hijos.
+													</li>
+												</ul>
 											</p>
 								    	</div>
 								    </div>
@@ -110,7 +118,7 @@
 								    			<a href='https://twitter.com/share' class='twitter-share-button' data-size='large' data-show-count='true'>Tweet</a><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
 								    		</div> -->
 								    		<hr class=''>
-								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-11-31 13:53:10</p>
+								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-11-31 14:15:10</p>
 								    	</div>
 								    </div>
 		    					</div>
