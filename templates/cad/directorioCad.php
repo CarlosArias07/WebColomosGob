1dbcfa867e06508d8aebf7559e1055a2ed13dc5b
admagu <adrianamzgz1@gmail.com>
2017-12-07 20:57:00 -0600

    cultura arte y deporte

---
+++ templates/cad/directorioCad.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<title>CETI COLOMOS</title>
+		<!--  META TAGS -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+		<!--  ESTILOS  -->
+        <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../../css/carousel.css">
+        <link rel="stylesheet" type="text/css" href="../../css/dropdown.css">
+        <link rel="stylesheet" type="text/css" href="../../css/imagegallery.css">
+		<link rel="stylesheet" type="text/css" href="../../css/principales.css">
+        <!-- Bootstrap Dropdown Hover CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+	</head>
+
+	<body>
+        <!-- MENU PRINCIPAL -->
+        <?php include_once('../../menu.html'); ?>
+
+        <!-- CONTENIDO A PARTIR DE AQUI -->
+        <main class="principal">
+        
+        <!--  CONTENIDO PÁGINA -->
+         
+		  <div class='container nota interior ficha'>
+		    					<div class='col-md-12 col-sm-12 col-xs-12'>
+					                <ol class='breadcrumb'>
+					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
+					                    <li><a href='../../index.php'>Inicio</a></li>
+										<li><a href='/WebColomosGob/templates/cad/cad.php'>CAD</a></li>
+					                    <li class='active'><a href='/WebColomosGob/templates/cad/directorioCad.php'>Directorio</a></li>
+										
+					                </ol>
+					            </div>
+								<div class='row'><div class='col-sm-7 pull-left'>
+					            		<h2 class='bottom-buffer'>Directorio CAD</h2>
+
+					            	</div>
+					            	<div class='col-md-4 col-xs-12 pull-right '>
+									    <!--imagen por encima del menu lateral-->
+					            		<img class='img img-responsive' src="../../imagenes/cad/cad.jpg" alt="CAD">
+										<!--Menu lateral -->
+										<div class='border-box submenu_relacionados'>
+									        <h4 class='bottom-buffer'>Submenú de la sección</h4>
+									        <hr class='red sitios_relacionados'>
+									       	<ul type="circle"><li><a href="/WebColomosGob/templates/cad/cad.php">CAD</a><hr class="sub-hr"></li>
+											                   <li><a href="/WebColomosGob/templates/cad/talleresCad.php" >Talleres y Galería</a><hr class="sub-hr"></li>	
+											                   <li><a href="/WebColomosGob/templates/cad/revistaConexion.php">Revistas Conexion</a><hr class="sub-hr"></li>               																
+											</ul>
+										  <!--<h4 class='bottom-buffer'>Sitios relacionados</h4>
+								            <hr class='red sitios_relacionados'>
+											<ul>
+											<li><a href="http://www.colomos.ceti.mx/">Plantel Colomos</a><hr class="sub-hr"></li>	
+											<li><a href="http://www.tonala.ceti.mx/">Plantel Tonalá</a><hr class="sub-hr"></li>	
+											<li><a href="http://coeston.ceti.mx/riosantiago">Plantel Río Santiago</a><hr class="sub-hr"></li>
+											</ul>-->
+									    </div> 
+
+								        <!--<section class='border-box'>
+								          <dl>
+								  	      	<dt>Autor</dt>
+								  	      	<dd>Centro de Enseñanza Técnica Industrial Plantel Colomos</dd>
+								  	      	<dt>Fecha de publicación</dt>
+								  	      	<dd>2017-10-20 18:40:15</dd>
+								          </dl>
+								        </section>-->
+								    </div>
+
+									<div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<hr class='red'>           
+                                           <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+											  <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/VictorJosue.png" alt="Victor" width="170" height="60">
+                                                  <div class="caption" >
+                                                    <h3>Víctor Josué Cabrera Huerta </h3>
+                                                    <p>Coordinador de Cultura Arte y Deporte, profesor de Acroyoga</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+       
+                                
+										  
+										  <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+											<br>
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/GilGarcia.png" alt="Gil" width="130" height="55">
+                                                  <div class="caption" >
+                                                    <h3>Jesús Gil García Galindo </h3>
+                                                    <p>Técnico de Sonido Vespertino y Administrador de Eventos</p>
+													
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+											   
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/AlfredoOrozco.png" alt="alfredo" width="150" height="50">
+                                                  <div class="caption" >
+                                                    <h3>Alfredo Orozco Mendoza </h3>
+                                                    <p>Profesor de Teatro y Cinematografía, Director de Teatro y Orquesta de Cámara</p>
+												   </div>
+                                             </div>
+                                           </div>
+										   
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+											 <br>
+                                               <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/denisseContreras.jpg" alt="denisse" width="130" height="40">
+                                                  <div class="caption" >
+                                                    <h3>Denisse Contreras Gómez </h3>
+                                                    <p>Profesora de Pintura y  profesora de Dibujo </p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/EdgarGiovanni.png" alt="cad" width="145" height="45">
+                                                  <div class="caption" >
+                                                    <h3>Edgar Giovanni Pardo Roldan </h3>
+                                                    <p>Profesor de Música (Canto y Percusiones)</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/JorgeGuerrero.png" alt="cad" width="95" height="20">
+                                                  <div class="caption" >
+                                                    <h3>Jorge Guerrero Benitez</h3>
+                                                    <p>Profesor de Danza Aérea</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/SebastianCecillon.png" alt="cad" width="145" height="45">
+                                                  <div class="caption" >
+                                                    <h3>Sebastián Cecillón Arévalo</h3>
+                                                    <p>Coordinador de Radio CETI</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/IrisAvelar.png" alt="cad" width="70" height="18">
+                                                  <div class="caption" >
+                                                    <h3> Iris  Avelar  López </h3>
+                                                    <p>Profesor de acondicionamiento Físico, Balón Mano y Defensa Personal</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									           <br>
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/CarlosSilva.png" alt="cad" width="95" height="20">
+                                                  <div class="caption" >
+                                                    <h3>Juan Carlos Silva Rosales</h3>
+                                                    <p>Profesor de Atletismo y Fútbol</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+										   <div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/ManuelIgnacio.png" alt="cad" width="160" height="50">
+                                                  <div class="caption" >
+                                                    <h3>Manuel Ignacio Reséndiz López </h3>
+                                                    <p>Profesor de Voleibol y de Baloncesto</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/LauraFregozo.png" alt="cad" width="70" height="25">
+                                                  <div class="caption" >
+                                                    <h3>Laura Alicia Fregozo Cuarenta </h3>
+                                                    <p>Profesor de Baile Hawaiano</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+									           <br>
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/nataliBonilla.png" alt="cad" width="130" height="40">
+                                                  <div class="caption" >
+                                                    <h3>Natali Bonilla Naranjo</h3>
+													
+                                                    <p>Profesor de Natación y Baloncesto</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+							
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/LuisSanchez.png" alt="cad" width="90" height="30">
+                                                  <div class="caption" >
+                                                    <h3>José Luis Sánchez Pérez</h3>
+                                                    <p>Profesor de Futbol</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+											
+											<div class="col-md-6 ">
+                                             <div class="thumbnail" style="border:2px solid #A4A4A4; background-color:#F2F2F2">
+							
+											   <img class="img img-responsive img-circle" src="/WebColomosGob/imagenes/cad/JuanGutierrez.png" alt="cad" width="90" height="30">
+                                                  <div class="caption" >
+                                                    <h3>Juan Francisco Javier Gutiérrez Cedillo</h3>
+                                                    <p>Profesor de fotografía</p>
+                                                   </div>
+                                             </div>
+                                           </div>
+										   
+								    	</div>
+								    </div>
+									
+									
+
+
+								 <div class='col-sm-7 pull-left'>
+								    	<div class='article-body'>
+								    		<!--<div class='sociales'>
+								    			<div class='fb-share-button' data-href='http://ceti.mx/noticia.php?id=48' data-layout='button_count' data-size='large' data-mobile-iframe='true'><a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fceti.mx%2Fnoticia.php%3Fid%3D48&amp;src=sdkpreparse'>Compartir</a></div>
+								    		</div>
+								    		<div class='sociales'>
+								    			<a href='https://twitter.com/share' class='twitter-share-button' data-size='large' data-show-count='true'>Tweet</a><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
+								    		</div> -->
+								    		<hr class=''>
+								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-12-07 19:51:10</p>
+								    	</div>
+								    </div>
+		    					</div>
+
+	    					</div>
+		<br>
+        <br>
+        <br>
+        </main>
+
+        <!-- FINAL CONTENIDO -->
+		<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+        <script src="js/jquery.js"></script>
+
+        <!-- Dropdown -->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Bootstrap Dropdown Hover JS -->
+        <script src="js/bootstrap-dropdownhover.min.js"></script>
+	</body>
+</html>
