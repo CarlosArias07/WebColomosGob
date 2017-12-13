03c851f556df2843bc7f70ab5a0fc81aa20c9028
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-12-13 14:50:29 -0600

    Noticias Avances

--- noticias.php
+++ noticias.php
@@ -2,90 +2,110 @@
 <html lang="es">
 
 	<head>
-		<title>CETI COLOMOS</title>
+		<title>TITULO</title>
 		<!--  META TAGS -->
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
 		<!--  ESTILOS  -->
         <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="../../css/carousel.css">
-        <link rel="stylesheet" type="text/css" href="../../css/dropdown.css">
-        <link rel="stylesheet" type="text/css" href="../../css/imagegallery.css">
-		<link rel="stylesheet" type="text/css" href="../../css/principales.css">
-        <!-- Bootstrap Dropdown Hover CSS -->
-    <link href="css/animate.min.css" rel="stylesheet">
-    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/lista.css">
 	</head>
 
 	<body>
-        <!-- MENU PRINCIPAL -->
-        <?php include_once('../../menu.html'); ?>
-
         <!-- CONTENIDO A PARTIR DE AQUI -->
-        <main class="principal">
+            <?php include_once('menu.html'); ?>
+            
+            <main class="principal">
         
         <!--  CONTENIDO PÁGINA -->
          
-		    <div class='container nota interior ficha'>
-		    					<div class='col-md-12 col-sm-12 col-xs-12'>
-					                <ol class='breadcrumb'>
-					                    <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
-					                    <li><a href='../../index.php'>Inicio</a></li>
-					                    <li>Alumnos</a></li>
-										<li class='active'><a href='/WebColomosGob/templates/alumnos/becas.php'>Becas</a></li>
-										             
-					                </ol>
-					            </div>
-								<div class='row'><div class='col-sm-7 pull-left'>
-					            		<h2 class='bottom-buffer'>Becas Nivel Tecnólogo e Ingeniería</h2>
-					            	</div>
-					            	<div class='col-md-4 col-xs-12 pull-right '>
-								    </div>
+            <div class='container nota interior ficha'>
+                                <div class='col-md-12 col-sm-12 col-xs-12'>
+                                    <ol class='breadcrumb'>
+                                        <li><a href='../../index.php'><i class='icon icon-home'></i></a></li>
+                                        <li><a href='../../index.php'>Inicio</a></li>
+                                        <li>Alumnos</a></li>
+                                        <li class='active'><a href='/WebColomosGob/templates/alumnos/becas.php'>Becas</a></li>
+                                                     
+                                    </ol>
+                                </div>
+                                <div class='row'><div class='col-sm-7 pull-left'>
+                                        <h2 class='bottom-buffer'>Becas Nivel Tecnólogo e Ingeniería</h2>
+                                    </div>
+                                    <div class='col-md-4 col-xs-12 pull-right '>
+                                    </div>
 
-								    <div class='col-sm-7 pull-left'>
-								    	<div class='article-body'>
-								    		<hr class='red'>
+                                    <div class='col-sm-7 pull-left'>
+                                        <div class='article-body'>
+                                            <hr class='red'>
 
-											<div class="col-md-14"><a href="/2-principal/199-beca-de-manutención" target="_self"><img class="img img-responsive" src="../../imagenes/alumnos/tecnologo.png" alt="Recorte becas largo 2017" width="100%" height="200px" /></a>&nbsp;</div>
+                                            <ul id="myList">
+                                                <li>One</li>
+                                                <li>Two</li>
+                                                <li>Three</li>
+                                                <li>Four</li>
+                                                <li>Five</li>
+                                                <li>Six</li>
+                                                <li>Seven</li>
+                                                <li>Eight</li>
+                                                <li>Nine</li>
+                                                <li>Ten</li>
+                                                <li>Eleven</li>
+                                                <li>Twelve</li>
+                                                <li>Thirteen</li>
+                                                <li>Fourteen</li>
+                                                <li>Fifteen</li>
+                                                <li>Sixteen</li>
+                                                <li>Seventeen</li>
+                                                <li>Eighteen</li>
+                                                <li>Nineteen</li>
+                                                <li>Twenty one</li>
+                                                <li>Twenty two</li>
+                                                <li>Twenty three</li>
+                                                <li>Twenty four</li>
+                                                <li>Twenty five</li>
+                                            </ul>
+                                            <div id="loadMore">Load more</div>
+                                            <div id="showLess">Show less</div>
 
-											<div class="col-md-14"><a href="/2-principal/207-resumen-beca-para-prácticas" target="_self"><img class="img img-responsive" src="../../imagenes/alumnos/ingenieria.png" alt="Recorte ch practicas 2016" width="100%" height="200px" /></a>&nbsp;</div>
-
-											<p class='contenido'>
-											Cualquier duda o aclaración sobre los diferentes Programas de BECAS acude a Trabajo Social, que se ubican en el Edificio A Planta Baja en horario de lunes a viernes en horario de 9:00 a 17:00 hrs. o comunícate al Teléfono 36413250 ext.503
-											</p>
-
-										</div>
-								 <div class='col-sm-7 pull-left'>
-								    	<div class='article-body'>
-								    		<!--<div class='sociales'>
-								    			<div class='fb-share-button' data-href='http://ceti.mx/noticia.php?id=48' data-layout='button_count' data-size='large' data-mobile-iframe='true'><a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fceti.mx%2Fnoticia.php%3Fid%3D48&amp;src=sdkpreparse'>Compartir</a></div>
-								    		</div>
-								    		<div class='sociales'>
-								    			<a href='https://twitter.com/share' class='twitter-share-button' data-size='large' data-show-count='true'>Tweet</a><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
-								    		</div> -->
-								    		<hr class=''>
-								    		<p class='ultima_actualizacion'>Ultima actualización: 2017-10-17 19:49:10</p>
-								    	</div>
-								    </div>
-		    					</div>
-	    	</div>
-		<br>
+                                        </div>
+                                 <div class='col-sm-7 pull-left'>
+                                        <div class='article-body'>
+                                            <!--<div class='sociales'>
+                                                <div class='fb-share-button' data-href='http://ceti.mx/noticia.php?id=48' data-layout='button_count' data-size='large' data-mobile-iframe='true'><a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fceti.mx%2Fnoticia.php%3Fid%3D48&amp;src=sdkpreparse'>Compartir</a></div>
+                                            </div>
+                                            <div class='sociales'>
+                                                <a href='https://twitter.com/share' class='twitter-share-button' data-size='large' data-show-count='true'>Tweet</a><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
+                                            </div> -->
+                                            <hr class=''>
+                                            <p class='ultima_actualizacion'>Ultima actualización: 2017-10-17 19:49:10</p>
+                                        </div>
+                                    </div>
+                                </div>
+            </div>
+        <br>
         <br>
         <br>
         </main>
 
         <!-- FINAL CONTENIDO -->
 		<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
-        <script src="js/jquery.js"></script>
-
-        <!-- Dropdown -->
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-
-        <!-- Bootstrap Dropdown Hover JS -->
-        <script src="js/bootstrap-dropdownhover.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function () {
+                size_li = $("#myList li").size();
+                x=3;
+                $('#myList li:lt('+x+')').show();
+                $('#loadMore').click(function () {
+                    x= (x+5 <= size_li) ? x+5 : size_li;
+                    $('#myList li:lt('+x+')').show();
+                });
+                $('#showLess').click(function () {
+                    x=(x-5<0) ? 3 : x-5;
+                    $('#myList li').not(':lt('+x+')').hide();
+                });
+            });
+        </script>
 	</body>
 </html>
